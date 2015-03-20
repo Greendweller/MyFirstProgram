6f82dc66accbc372c72cc47f6cf6bec74f19a2b8
Greendweller <robbert_oorschot@hotmail.com>
2015-03-20 16:58:08 +0100

    Making propper header files
    Okay so i placed execuatble code in the headers and thats not conform
    the C guidelines.  I still have to do the other headers but this is a
    start.

---
+++ menu.c
@@ -0,0 +1,16 @@
+//* Gemaakt door R.Oorschot 2015 als test om te leren hoe C nou precies werkt *//
+#include <stdio.h>
+#include "menu.h"
+
+void menu()
+{
+    printf(" \n Gemaakt door R.Oorschot 2015.\n");
+    printf(" ----------------------------------------\n\n");
+    printf(" 1. Cirkelmaten berekenen. \n");
+    printf(" 2. \n");
+    printf(" 3. \n");
+    printf(" 9. Scherm wissen. \n");
+    printf(" 0. Exit. \n \n");
+    printf(" Maak uw keuze en druk vervolgens op enter: ");
+}
+
