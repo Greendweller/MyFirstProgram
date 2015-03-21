785e12d2f29033f4e3bf835c59d31fd86730ccc0
Greendweller <robbert_oorschot@hotmail.com>
2015-03-21 09:29:21 +0100

    Input:  Added a user input validation check
    Added a verry simple user input validation code.
    It does work however it also introduced a bug.
    
    When a user puts an invalid choise the program bugs out. The correct
    choise lets it work as intended.

--- input/input.c
+++ input/input.c
@@ -1,13 +1,16 @@
 //* Made by R.Oorschot 2015 to test and learn the C programming language *//
 #include <stdio.h>
+#include <stdlib.h>
 #include "input.h"
 #include "../circle/circlefunctions.h"
 
 int menu_user_input()
 {
     int number;
-    scanf(" %d", &number);
-    return number;
+    if (scanf(" %d", &number) !=1);
+    printf("\n ***Not a valid menu choise*** ");
+
+        return number;
 }
 
 float cir_user_input()
