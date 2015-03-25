279366e66710048e17388c306b31e234bf99d9bb
Greendweller <robbert_oorschot@hotmail.com>
2015-03-25 11:17:48 +0100

    Menu:  renamed options
    Renamed options to reflect what the actually do to avoid confusion.

--- menu/menu.c
+++ menu/menu.c
@@ -8,9 +8,9 @@ void menu()
     printf(" \n Made by Greendweller 2015.\n");
     printf(" ----------------------------------------\n\n");
     printf(" 1. Calculate circle measurements. \n");
-    printf(" 2. \n");
-    printf(" 3. \n");
-    printf(" 8. Info. \n");
+    printf(" 2. Clear screen. \n");
+    printf(" 3. Clear screen. \n");
+    printf(" 8. Clear screen. \n");
     printf(" 9. Clear screen. \n");
     printf(" 0. Exit. \n \n");
     printf(" Make your choice and press enter: ");
